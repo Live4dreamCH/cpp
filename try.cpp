bc770410818bf3b32a747f165599754be0c5cb76
Live4dreamCH <live4dream@stu.xjtu.edu.cn>
2020-02-26 22:51:02 +0800

    2

--- try.cpp
+++ try.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
-#include <string>
+#include <cmath>
 using namespace std;
 
 int main()
 {
-    string a;
-    getline(cin, a); //可以接受空格、tab
-    cout << a;
+    long long a;
+    long long t = 2;
+    cin >> a;
+    cout << 1 << ' ';
+    while (a)
+    {
+        if (a % t == 0)
+        {
+            a /= t;
+            cout << t << ' ';
+        }
+        else if(t<a)
+        {
+            t++;
+        }
+        else
+            break;
+    }
     return 0;
 }
+
+// #include <string>
+// string a;
+// getline(cin, a); //可以接受空格、tab
+// cout << a;
